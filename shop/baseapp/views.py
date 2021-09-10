238eefe08ae121bbc7c749e7b1654273b718662c
ZhanatUturova <63417019+ZhanatUturova@users.noreply.github.com>
2021-09-11 01:09:31 +0600

    22 lesson add cartview and cart.html

--- shop/baseapp/views.py
+++ shop/baseapp/views.py
@@ -3,7 +3,7 @@ from django.db.models.base import Model
 from django.shortcuts import render
 from django.views.generic import DetailView, View
 
-from .models import Notebook, Smartphone, Category, LatestProducts
+from .models import Customer, Notebook, Smartphone, Category, LatestProducts, Cart
 from .mixins import CategoryDetailMixin
 
 
@@ -46,3 +46,16 @@ class CategoryDetailView(CategoryDetailMixin, DetailView):
     context_object_name = 'category'
     template_name = 'category_detail.html'
     slug_url_kwarg = 'slug'
+
+
+class CartView(View):
+
+    def get(self, request, *args, **kwargs):
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer)
+        categories = Category.objects.get_categories_for_left_sidebar()
+        context = {
+            'cart': cart,
+            'categories': categories
+        }
+        return render(request, 'cart.html', context)
