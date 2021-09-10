31c32b004888bfcb2f8a49ea8d530e89f840c324
ZhanatUturova <63417019+ZhanatUturova@users.noreply.github.com>
2021-09-10 23:37:58 +0600

    21 lesson add products into mainpage

--- shop/baseapp/views.py
+++ shop/baseapp/views.py
@@ -3,7 +3,7 @@ from django.db.models.base import Model
 from django.shortcuts import render
 from django.views.generic import DetailView, View
 
-from .models import Notebook, Smartphone, Category
+from .models import Notebook, Smartphone, Category, LatestProducts
 from .mixins import CategoryDetailMixin
 
 
@@ -11,12 +11,14 @@ class BaseView(View):
 
     def get(self, request, *args, **kwargs):
         categories = Category.objects.get_categories_for_left_sidebar()
-        return render(request, 'base.html', {'categories': categories})
-
-
-# def test_view(request):
-#     categories = Category.objects.get_categories_for_left_sidebar()
-#     return render(request, 'base.html', {'categories': categories})
+        products = LatestProducts.objects.get_products_for_main_page(
+            'notebook', 'smartphone', with_respect_to='notebook'
+            )
+        context = {
+            'categories': categories,
+            'products': products
+        }
+        return render(request, 'base.html', context)
 
 
 class ProductDetailView(CategoryDetailMixin, DetailView):
@@ -32,8 +34,6 @@ class ProductDetailView(CategoryDetailMixin, DetailView):
         self.queryset = self.model._base_manager.all()
         return super().dispatch(request, *args, **kwargs)
 
-    # model = Model
-    # queryset = Model.objects.all()
     context_object_name = 'product' # product - передается в шаблоны, таким образом мы можем работать с шаблоном через product и вызывать все атрибуты, которые есть в модели, так как сама вьюшка ProductDetailView связана с соответствующей моделью (Notebook/Smartphone)
     template_name = 'product_detail.html'
     slug_url_kwarg = 'slug'
